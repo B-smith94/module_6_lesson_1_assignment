62c153d079224509f2aa76e0858ba70d0b4f7981
b-smith94 <byrondsmith9@gmail.com>
2024-09-24 11:14:41 -0400

    Added second version of fetch_planet_date that only returns the name and mass of planets. Completed function to find the heaviest planet from this data.

--- fetch_planet_data.py
+++ fetch_planet_data.py
@@ -1,25 +1,41 @@
 import requests
 import json
 
-#Task 2 and 3
+#Task 2
+
+# def fetch_planet_data():
+#     url = "https://api.le-systeme-solaire.net/rest/bodies/"
+#     response = requests.get(url)
+#     planets = response.json()['bodies']
+
+#     #process each planet info
+#     for planet in planets:
+#         if planet['isPlanet']:
+#             name = planet['englishName']
+#             mass = planet['mass']['massValue']
+#             orbit_period = planet['sideralOrbit']
+#             print(f"Planet: {name}\n   Mass: {mass}\n   Orbit Period: {orbit_period} days")
+
+# fetch_planet_data()
 
 def fetch_planet_data():
     url = "https://api.le-systeme-solaire.net/rest/bodies/"
     response = requests.get(url)
     planets = response.json()['bodies']
 
-    #process each planet info
+    planet_dict = {}
     for planet in planets:
         if planet['isPlanet']:
             name = planet['englishName']
-            mass = planet['mass']
-            orbit_period = planet['sideralOrbit']
-            return f"Planet: {name}\n   Mass: {mass}\n   Orbit Period: {orbit_period} days"
+            mass = planet['mass']['massValue']
+            planet_dict.update({name: mass})
+    return planet_dict
 
-def find_heaviest_planet(planet_list):
-    pass
-
+def find_heaviest_planet(planets):
+    heaviest_planet = max(planets, key = planets.get)
+    return heaviest_planet, planets[heaviest_planet]
+            
 planets = fetch_planet_data()
 
-name, mass= find_heaviest_planet(planets)
+name, mass = find_heaviest_planet(planets)
 print(f"The heaviest planet is {name} with a mass of {mass} kg.")
