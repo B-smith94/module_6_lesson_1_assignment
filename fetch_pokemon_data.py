7415dc637c42468f5ea2395528908c9c1e51a401
b-smith94 <byrondsmith9@gmail.com>
2024-09-23 20:17:43 -0400

    Finished work to retrieve pokemon data and calculate the average weight of a list of pokemon.

--- fetch_pokemon_data.py
+++ fetch_pokemon_data.py
@@ -2,7 +2,13 @@ import requests
 import json
 #Task 1: See myenv
 
-#Task 2 and 3:
+#Task 2:
+response = requests.get(f'https://www.pokeapi.co/api/v2/pokemon/pikachu')
+json_data = response.text
+pokemon_data = json.loads(json_data)
+print(f"Name: {pokemon_data['name']}\n{pokemon_data['abilities']}")
+
+#Task 3:
 
 def fetch_pokemon_data(pokemon_name):
     response = requests.get(f'https://www.pokeapi.co/api/v2/pokemon/{pokemon_name}')
@@ -12,11 +18,12 @@ def fetch_pokemon_data(pokemon_name):
 
 
 def calculate_average_weight(pokemon_list):
+    pokemon_weight = []
     for name in pokemon_list:
         pokemon_data = fetch_pokemon_data(name)
-    return sum(pokemon_data["weight"]) / len(pokemon_data["weight"])
+        pokemon_weight.append(pokemon_data[-1])
+    return sum(pokemon_weight) / len(pokemon_weight)
 
 pokemon_names = ["pikachu", "bulbasaur", "charmander"]
 
-print(calculate_average_weight(pokemon_names))
-
+print(f"Average Weight of {pokemon_names}: {calculate_average_weight(pokemon_names)}")
