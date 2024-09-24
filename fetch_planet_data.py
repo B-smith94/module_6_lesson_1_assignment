6132192ed93efc59e74a6249207abbbc1360d3f8
b-smith94 <byrondsmith9@gmail.com>
2024-09-24 11:46:54 -0400

    Edited comments for clarity of which task was completed

--- fetch_planet_data.py
+++ fetch_planet_data.py
@@ -2,20 +2,22 @@ import requests
 
 #Task 2
 
-# def fetch_planet_data():
-#     url = "https://api.le-systeme-solaire.net/rest/bodies/"
-#     response = requests.get(url)
-#     planets = response.json()['bodies']
+def fetch_planet_data():
+    url = "https://api.le-systeme-solaire.net/rest/bodies/"
+    response = requests.get(url)
+    planets = response.json()['bodies']
 
-#     #process each planet info
-#     for planet in planets:
-#         if planet['isPlanet']:
-#             name = planet['englishName']
-#             mass = planet['mass']['massValue']
-#             orbit_period = planet['sideralOrbit']
-#             print(f"Planet: {name}\n   Mass: {mass}\n   Orbit Period: {orbit_period} days")
+    #process each planet info
+    for planet in planets:
+        if planet['isPlanet']:
+            name = planet['englishName']
+            mass = planet['mass']['massValue']
+            orbit_period = planet['sideralOrbit']
+            print(f"Planet: {name}\n   Mass: {mass}\n   Orbit Period: {orbit_period} days")
 
-# fetch_planet_data()
+fetch_planet_data()
+
+#Task 3
 
 def fetch_planet_data():
     url = "https://api.le-systeme-solaire.net/rest/bodies/"
